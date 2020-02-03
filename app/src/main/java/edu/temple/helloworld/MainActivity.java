da9e7ee8666f287dd9b59882c6f3a84d112064e5
Adam Gasiewski <agasiewski17@gmail.com>
2020-02-03 00:29:55 -0500

    Added button, added necessary code, and centered it horizontally.

--- app/src/main/java/edu/temple/helloworld/MainActivity.java
+++ app/src/main/java/edu/temple/helloworld/MainActivity.java
@@ -3,12 +3,33 @@ package edu.temple.helloworld;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    Button changeTextButton;
+    TextView message;
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        message = (TextView)findViewById(R.id.textView);
+        changeTextButton = (Button)findViewById(R.id.ChangeText);
+        changeTextButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                message.setText("You have clicked the button");
+            }
+        } );
+
     }
 }
